b12ee11e1f8d887eca44f66ee0ad3990fc8ef7d9
zhamid01 <zhamid01@uw.edu>
2020-11-14 22:32:22 -0800

    Working on InsertInBST

--- src/main/java/Problem3/InsertInBST.java
+++ src/main/java/Problem3/InsertInBST.java
@@ -4,6 +4,9 @@ import Problem1.TreeNode;
 
 public class InsertInBST {
     public static void insert(TreeNode<Integer> root, int valToInsert) {
-        // homework
+        if (root == null) {
+            root = new TreeNode<Integer>(valToInsert);
+            return;
+        }
     }
 }
